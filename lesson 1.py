3d964e4e87350e275fe955ed02c7739cec35dc87
Ertyxis <123876238+Ertyxis@users.noreply.github.com>
2023-04-06 14:05:51 +0300

    update
    basic trainig

---
+++ lesson 1.py
@@ -0,0 +1,26 @@
+x = 5
+print(x)
+first_name = "John"
+last_name = "Doe" 
+full_name = first_name + " " + last_name
+print(full_name)
+x = 5
+if x > 0:
+    print("Positive")
+elif x <0:
+    print("Negative")
+else:
+    print("Zero")
+def add_number(x, y):
+    return x + y
+result = add_number(3, 4)
+print(result)
+my_list = [1,2,3,4,5]
+print(my_list[2])
+my_list = [1,2,3,4,5]
+my_list.append(6)
+print(my_list[2])
+first_name1 = "Lukas"
+last_name1 = "Sobczyk"
+full_name1 = first_name1 + " " + last_name1
+print(full_name1)
